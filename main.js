19d36f02f8f46110c4e6705a601a39c53268a81a
asmape0104 <6086632642@g.ecc.u-tokyo.ac.jp>
2022-05-20 16:42:05 +0900

    add: platform and announce

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
-const { ref, computed } = Vue
+const { ref, computed, onMounted } = Vue
+
+const generateAnnounce = () => {
+  const trackNumber = Math.floor(Math.random() ** 2.5 * 20) + 1
+
+  const messages = [
+    `まもなく、${trackNumber}番線に、通勤快速、成東行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。この列車は、10両です。`,
+    `まもなく、${trackNumber}番線に、快速、蘇我行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。この列車は、10両です。`,
+    `まもなく、${trackNumber}番線に、東京、上野方面行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。次は、高輪ゲートウェイ、に止まります。`,
+    `まもなく、${trackNumber}番線に、新宿、渋谷方面行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。次は、新宿、に止まります。`,
+    `まもなく、${trackNumber}番線に、各駅停車、桜木町行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。`,
+    `まもなく、${trackNumber}番線に、各駅停車、大宮行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。`,
+    `まもなく、${trackNumber}番線に、快速、大船行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。`,
+    `まもなく、${trackNumber}番線に、各駅停車、武蔵浦和行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。`,
+    `まもなく、${trackNumber}番線に、りんかい線直通、各駅停車、新木場行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。`,
+    `まもなく、${trackNumber}番線に、りんかい線直通、快速、新木場行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。次は、武蔵浦和に止まります。北戸田、戸田、浮間舟渡、北赤羽、には、停車いたしません。`,
+    `まもなく、${trackNumber}番線に、上野東京ライン、東海道線直通、普通、沼津行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。この電車は、15両です。`,
+    `まもなく、${trackNumber}番線に、上野東京ライン、宇都宮線直通、普通、宇都宮行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。この電車は、15両です。`,
+    `まもなく、${trackNumber}番線に、上野東京ライン、高崎線直通、普通、高崎行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。この電車は、15両です。`,
+    `まもなく、${trackNumber}番線に、湘南新宿ライン、横須賀線直通、普通、逗子行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。この電車は、10両です。`,
+    `まもなく、${trackNumber}番線に、快速、土浦行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。この電車は、15両です。次は、松戸に止まります。`,
+    `まもなく、${trackNumber}番線に、各駅停車、千葉行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。`,
+    `まもなく、${trackNumber}番線に、快速、青梅行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。`,
+    `まもなく、${trackNumber}番線に、中央特快、高尾行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。`,
+    `まもなく、${trackNumber}番線に、久里浜行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。この電車は、11両です。`,
+    `まもなく、${trackNumber}番線に、快速、君津行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。この電車は、15両です。`,
+    `まもなく、${trackNumber}番線に、快速、成田空港、成東行きが、参ります。危ないですから、黄色い点字ブロックまで、お下がりください。この電車は、15両です。`,
+  ]
+
+  return messages[Math.floor(Math.random() * messages.length)]
+}
 
 const SCube = {
   props: {
@@ -25,20 +55,16 @@ const SCube = {
     zl: {
       type: Number,
       default: 0
-    },
-    cameraDeg: {
-      type: Number,
-      default: 0
     }
   },
   template: `
     <div style="transform-style: preserve-3d;">
-      <div :style="{position: 'absolute', top: \`calc(50vh - \${yl/2}px)\`, left: \`calc(50vw - \${xl/2}px)\`, width: \`\${xl}px\`, height: \`\${yl}px\`, transform: \`rotateX(85deg) rotateZ(\${cameraDeg}deg) translate3d(\${x}px, \${y}px, \${z}px) rotateY(  0deg)                 translateZ(\${zl/2}px)\`}"><slot name="top"/></div>
-      <div :style="{position: 'absolute', top: \`calc(50vh - \${yl/2}px)\`, left: \`calc(50vw - \${xl/2}px)\`, width: \`\${xl}px\`, height: \`\${yl}px\`, transform: \`rotateX(85deg) rotateZ(\${cameraDeg}deg) translate3d(\${x}px, \${y}px, \${z}px) rotateY(180deg)                 translateZ(\${zl/2}px)\`}"><slot name="bottom"/></div>
-      <div :style="{position: 'absolute', top: \`calc(50vh - \${zl/2}px)\`, left: \`calc(50vw - \${xl/2}px)\`, width: \`\${xl}px\`, height: \`\${zl}px\`, transform: \`rotateX(85deg) rotateZ(\${cameraDeg}deg) translate3d(\${x}px, \${y}px, \${z}px) rotateX(-90deg)                 translateZ(\${yl/2}px)\`}"><slot name="front"/></div>
-      <div :style="{position: 'absolute', top: \`calc(50vh - \${zl/2}px)\`, left: \`calc(50vw - \${xl/2}px)\`, width: \`\${xl}px\`, height: \`\${zl}px\`, transform: \`rotateX(85deg) rotateZ(\${cameraDeg}deg) translate3d(\${x}px, \${y}px, \${z}px) rotateX(-90deg) rotateY(180deg) translateZ(\${yl/2}px)\`}"><slot name="back"/></div>
-      <div :style="{position: 'absolute', top: \`calc(50vh - \${zl/2}px)\`, left: \`calc(50vw - \${yl/2}px)\`, width: \`\${yl}px\`, height: \`\${zl}px\`, transform: \`rotateX(85deg) rotateZ(\${cameraDeg}deg) translate3d(\${x}px, \${y}px, \${z}px) rotateX(-90deg) rotateY( 90deg) translateZ(\${xl/2}px)\`}"><slot name="right"/></div>
-      <div :style="{position: 'absolute', top: \`calc(50vh - \${zl/2}px)\`, left: \`calc(50vw - \${yl/2}px)\`, width: \`\${yl}px\`, height: \`\${zl}px\`, transform: \`rotateX(85deg) rotateZ(\${cameraDeg}deg) translate3d(\${x}px, \${y}px, \${z}px) rotateX(-90deg) rotateY(-90deg) translateZ(\${xl/2}px)\`}"><slot name="left"/></div>
+      <div :style="{position: 'absolute', top: \`calc(50vh - \${yl/2}px)\`, left: \`calc(50vw - \${xl/2}px)\`, width: \`\${xl}px\`, height: \`\${yl}px\`, transform: \`translate3d(\${x}px, \${y}px, \${z}px) rotateY(  0deg)                 translateZ(\${zl/2}px)\`}"><slot name="top"/></div>
+      <div :style="{position: 'absolute', top: \`calc(50vh - \${yl/2}px)\`, left: \`calc(50vw - \${xl/2}px)\`, width: \`\${xl}px\`, height: \`\${yl}px\`, transform: \`translate3d(\${x}px, \${y}px, \${z}px) rotateY(180deg)                 translateZ(\${zl/2}px)\`}"><slot name="bottom"/></div>
+      <div :style="{position: 'absolute', top: \`calc(50vh - \${zl/2}px)\`, left: \`calc(50vw - \${xl/2}px)\`, width: \`\${xl}px\`, height: \`\${zl}px\`, transform: \`translate3d(\${x}px, \${y}px, \${z}px) rotateX(-90deg)                 translateZ(\${yl/2}px)\`}"><slot name="front"/></div>
+      <div :style="{position: 'absolute', top: \`calc(50vh - \${zl/2}px)\`, left: \`calc(50vw - \${xl/2}px)\`, width: \`\${xl}px\`, height: \`\${zl}px\`, transform: \`translate3d(\${x}px, \${y}px, \${z}px) rotateX(-90deg) rotateY(180deg) translateZ(\${yl/2}px)\`}"><slot name="back"/></div>
+      <div :style="{position: 'absolute', top: \`calc(50vh - \${zl/2}px)\`, left: \`calc(50vw - \${yl/2}px)\`, width: \`\${yl}px\`, height: \`\${zl}px\`, transform: \`translate3d(\${x}px, \${y}px, \${z}px) rotateX(-90deg) rotateY( 90deg) translateZ(\${xl/2}px)\`}"><slot name="right"/></div>
+      <div :style="{position: 'absolute', top: \`calc(50vh - \${zl/2}px)\`, left: \`calc(50vw - \${yl/2}px)\`, width: \`\${yl}px\`, height: \`\${zl}px\`, transform: \`translate3d(\${x}px, \${y}px, \${z}px) rotateX(-90deg) rotateY(-90deg) translateZ(\${xl/2}px)\`}"><slot name="left"/></div>
     </div>
   `
 }
@@ -107,15 +133,61 @@ const App = {
     SWindow
   },
   setup() {
-    const cameraDeg = ref(10)
+    const cameraDeg = ref(-20)
+    const cameraDeg2 = ref(80)
+    const trainPos = ref(0)
+
     const frontDoorOpen = ref(false)
     const backDoorOpen = ref(false)
     const lineColor = ref('#ff0000')
+    const cameraTransform = computed(() => `rotateX(${cameraDeg2.value}deg) rotateZ(${cameraDeg.value}deg)`)
+
+    const voices = ref(window.speechSynthesis.getVoices())
+    const selectedVoiceName = ref('')
+    onMounted(() => {
+      setTimeout(() => {
+        voices.value = window.speechSynthesis.getVoices()
+        // selectedVoiceName.value = (voices.value.find(v => v.name === 'Google 日本語') || voices.value[0]).name
+        selectedVoiceName.value = voices.value[0].name
+        window.speechSynthesis.cancel()
+      }, 1000)
+    })
+    const voiceNames = computed(() => voices.value.map(v => v.name))
+    const voiceVolume = ref(0.9)
+
+    const announceText = ref('まもなく、1番線に、通勤快速、成東行きが、参ります。。危ないですから、黄色い点字ブロックまで、お下がりください。。この列車は、10両です。。')
+    const playAnnounce = () => {
+      window.speechSynthesis.cancel()
+      const uttr = new SpeechSynthesisUtterance(announceText.value)
+      uttr.voice = voices.value.find(v => v.name === selectedVoiceName.value)
+      uttr.volume = voiceVolume.value
+      window.speechSynthesis.speak(uttr)
+    }
+
+    const stopAnnounce = () => {
+      window.speechSynthesis.cancel()
+    }
+
+    const randomAnnounce = () => {
+      announceText.value = generateAnnounce()
+    }
+
     return {
       cameraDeg,
+      cameraDeg2,
+      trainPos,
       frontDoorOpen,
       backDoorOpen,
-      lineColor
+      lineColor,
+      announceText,
+      voices,
+      voiceNames,
+      voiceVolume,
+      selectedVoiceName,
+      playAnnounce,
+      stopAnnounce,
+      randomAnnounce,
+      cameraTransform
     }
   }
 }
